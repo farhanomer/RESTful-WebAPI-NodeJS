80ce7d69fd806b448c5b93d05299fe3b6ae378cd
Farhan Omer <farhanomer@gmail.com>
2017-03-17 11:35:48 -0400

    Entity Model Creation

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 /**
  * Created by fomer2 on 3/16/2017.
  */
-var express =require('express');
+var express =require('express'),
+    mongoose=require('mongoose');
+var db=mongoose.connect('mongodb://localhost/entityAPI');
+var Entity=require('./models/entityModel');
+
 var  app=express();
 var port= process.env.PORT || 3000;
 
---
+++ models/entityModel.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Farhan Omer on 3/17/2017.
+ */
+var mongoose=require('mongoose'),
+    Schema=mongoose.Schema;
+var entityModel= new Schema({
+    UserName:{type: String},
+    Phone:{type: String},
+    read:{type: Boolean,default:false}
+});
+module.exports=mongoose.model('Entity',entityModel);
+
