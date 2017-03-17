04529854a8995aed5508911addc0982fd72c208c
Farhan Omer <farhanomer@gmail.com>
2017-03-17 09:26:27 -0400

    Entity Response Router

--- app.js
+++ app.js
@@ -4,9 +4,19 @@
 var express =require('express');
 var  app=express();
 var port= process.env.PORT || 3000;
+
+var entityRouter= express.Router();
+entity.route('/Entity').get(function(req,res){
+     var responseJSON={hello:"This is my api"};
+     res.json(responseJSON);
+
+});
+
+app.use('/api',router);
 app.get('/',function(req,res){
     res.send('Welcome to Restful API using gulp');
 });
+
 app.listen(port,function(){
    console.log('Running on PORT: '+ port);
 });
