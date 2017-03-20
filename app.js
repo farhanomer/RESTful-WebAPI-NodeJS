82b2b051c6fb2c6918a4a3c30b6228992bdcf4c5
Farhan Omer <farhanomer@gmail.com>
2017-03-20 16:06:42 -0400

    With Core Mongo DB

--- app.js
+++ app.js
@@ -8,44 +8,14 @@ var express =require('express'),
 var db=mongoose.connect('mongodb://localhost/entityAPI');
 var Entity=require('./models/entityModel');
 
+/**
+ *
+ */
+//var Connection = require('dbConfig');
+
 var  app=express();
 var port= process.env.PORT || 3000;
-
-/* mongoDB connection*/
-
-
-var MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://127.0.0.1:27017/entityDB", function (err, db) {
-        //console.log(db.getName());
-    if(err) throw err;
-        //alert(db);
-    //Write databse Insert/Update/Query code here..
-
-    console.log('Total Rows: ' + db.name());
-
-    db.collection('Persons', function (err, collection) {
-
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-
-
-
-
-        (function (err, count) {
-            if (err) throw err;
-
-            console.log('Total Rows: ' + count);
-        });
-    });
-});
-
-/*mongodb connectione ends here*/
-
-
-
+//Mangoose Connection
 var entityRouter= express.Router();
 entityRouter.route('/Entities').get(function(req,res){
      Entity.find(function(err,entities){
@@ -58,6 +28,11 @@ entityRouter.route('/Entities').get(function(req,res){
 
 
 });
+
+
+
+
+
 
 app.use('/api',entityRouter);
 app.get('/',function(req,res){
